1b4b655f5660cf7b2c5a55d3d4e526f525e4ae7b
Miguel Rojas <mrojasri@mrojas.emea.redhat>
2017-06-13 17:50:32 +0200

    database variables changed

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DB['server'] = $_ENV["MYSQL_SERVICE_HOST"];
+$DB['server'] = $_ENV["OPENSHIFT_MYSQL_DB_HOST"];
 $DB['user'] = $_ENV["DB_USERNAME"];
 $DB['password'] = $_ENV["DB_PASSWORD"];
 $DB['db'] = 'faq';

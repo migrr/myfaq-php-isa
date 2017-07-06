ba0aeb8a0f2037f12145eb4779dd9e54c50650a0
migrr <mrojasri@mrojas.emea.redhat>
2017-07-06 16:09:54 +0200

    updating env variables

--- phpmyfaq/config/database.php
+++ phpmyfaq/config/database.php
@@ -2,6 +2,6 @@
 $DB['server'] = $_ENV["MYSQL_DATABASE"];
 $DB['user'] = $_ENV["MYSQL_USER"];
 $DB['password'] = $_ENV["MYSQL_PASSWORD"];
-$DB['db'] = 'test';
+$DB['db'] = 'sampledb';
 $DB['prefix'] = '';
-$DB['type'] = 'mysqli';
+$DB['type'] = 'mysql';
